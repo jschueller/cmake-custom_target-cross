db50d8c1b3e8ca94306c9c304af8e8a01a26d3db
Julien Schueller <schueller@phimeca.com>
2019-10-27 14:58:16 +0100

    Simplify

--- FOOCompileToolsConfig.cmake
+++
@@ -1,4 +0,0 @@
-
-add_executable (fooConfigureMain IMPORTED)
-set_target_properties(fooConfigureMain PROPERTIES IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/configure.py")
-set_target_properties(fooConfigureMain PROPERTIES CROSSCOMPILING_EMULATOR "/usr/bin/python")
